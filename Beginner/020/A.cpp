b2a7dc5c351a24aeb440fed4d2aef2e3c852d3e4
Arusu <arse.mria@gmail.com>
2019-08-14 21:37:15 +0900

    A prog AC

---
+++ Beginner/020/A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int Q;
+
+    cin >> Q;
+    if(Q == 1){
+        cout << "ABC";
+    }else if(Q == 2){
+        cout << "chokudai";
+    }
+    cout << endl;
+    return 0;
+}
