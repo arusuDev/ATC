489e8c7861de7ef3cb54df7fe791f5732bcd16cf
Arusu <arse.mria@gmail.com>
2019-08-15 21:35:42 +0900

    A AC

---
+++ Beginner/A.cpp
@@ -0,0 +1,12 @@
+//21:32
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int  N;
+    cin >> N;
+    cout << N << endl;
+    for(int i=0;i<N;i++){
+        cout << 1 << endl;
+    }
+}
