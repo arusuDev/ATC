12ecb5dd3e84a0ade7d1e10a94b8e03b7f39f53d
arusuDev <arse.mria@gmail.com>
2019-08-22 17:54:33 +0900

    Add : hatena Update : C.cpp refactor

--- Beginner/026/C.cpp
+++ Beginner/026/C.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 class Employee{
     public:
-    int salary,No,boss,min,max;
+    int salary,boss,min,max;
     bool leaf;
     Employee(){
         min = -1;
@@ -34,26 +34,18 @@ class Employee{
         }
     }
 };
-
-bool cmp(Employee &a,Employee &b){
-    return a.boss < b.boss;
-}
-
 int main(void){
     int N;
     cin >> N;
     Employee* B = new Employee[N];
-    B[0].No = 1;
     B[0].boss = -1;//高橋くんがボス 
     for(int i=1;i<N;i++){
-        B[i].No = i+1;
         cin >> B[i].boss;
     }
     for(int i=N-1;i>0;i--){
         //cout << "社員番号：" << i;
         B[i].calcSal();
-        //cout << "給料　：　" << B[i].salary << "上司　：　" << B[i].boss << endl;
-
+       //cout << "給料　：　" << B[i].salary << "上司　：　" << B[i].boss << endl;
         B[B[i].boss-1].setSub(B[i].salary);
     }
     B[0].calcSal();
