4e98226167f1f70aa043d8ae0de8c6ebae6dd6d7
arusuDev <arse.mria@gmail.com>
2019-08-23 22:59:15 +0900

    A AC

---
+++ Beginner/027/A.cpp
@@ -0,0 +1,18 @@
+// C++のテンプレート
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void){
+    int l[3];
+    for(int i=0;i<3;i++)
+        cin >> l[i];
+    
+    if(l[0] == l[1]){
+        cout << l[2] << endl;    
+    }else if(l[1] == l[2]){
+        cout << l[0] << endl;
+    }else{
+        cout << l[1] << endl;
+    }
+}
